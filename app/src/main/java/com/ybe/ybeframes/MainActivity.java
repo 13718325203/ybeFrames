c0d9821012a1d962cf4c44e6cb485f06d67a3acc
mylihaihui <hui0109it@sina.com>
2017-10-16 10:09:43 +0800

    提交数据

---
+++ app/src/main/java/com/ybe/ybeframes/MainActivity.java
@@ -0,0 +1,17 @@
+package com.ybe.ybeframes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ *  http://blog.csdn.net/u013564742/article/details/52771740
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
---
+++ app/src/main/java/com/ybe/ybeframes/utils/ChinaUtils.java
@@ -0,0 +1,8 @@
+package com.ybe.ybeframes.utils;
+
+/**
+ * Created by hp on 2017/10/13.
+ */
+
+public class ChinaUtils {
+}
